140b011fe6db0a0171ff5457a33ad2cf1c2b3078
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-29 17:30:20 -0300

    Create 3.py
    1st v

---
+++ 3.py
@@ -0,0 +1,9 @@
+#3 Programa que pede dois números e imprime a soma.
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+a = float(input('Forneça um número real qualquer: '))
+b = float(input('Forneça um número real qualquer: '))
+soma = a + b
+
+print('Soma dos números: ', soma)
