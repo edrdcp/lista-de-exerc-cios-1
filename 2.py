0b2f7faa514ba4d3ac62c4630bee5f8756431aa2
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-29 17:29:02 -0300

    Create 2.py
    1st v

---
+++ 2.py
@@ -0,0 +1,7 @@
+#2 Programa que pede um número e então mostra a mensagem O número informado foi [número].
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+n = float(input('Forneça um número real qualquer: '))
+
+print('O número informado foi: ', n)
