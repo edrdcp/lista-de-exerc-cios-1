dfd7b658715d0a409e47714e0315587a5a33f1e4
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 16:56:33 -0300

    1st v

---
+++ 12.py
@@ -0,0 +1,7 @@
+#12
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+altura = float(input('Forneça sua altura em m: '))
+pesoIdeal = (72.7 * altura) - 58
+print('Seu peso ideal é: %.2f' %pesoIdeal)
