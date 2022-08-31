03cbd4d2ae9636a1e724318d22e273f03497cb1b
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 16:39:51 -0300

    Create 4.py
    1st v

---
+++ 4.py
@@ -0,0 +1,11 @@
+#4 Faça um Programa que pede as 4 notas bimestrais e mostra a média.
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+n1 = float(input('Forneça a primeira nota: '))
+n2 = float(input('Forneça a primeira nota: '))
+n3 = float(input('Forneça a primeira nota: '))
+n4 = float(input('Forneça a primeira nota: '))
+media = (n1 + n2 + n3 + n4) / 4
+
+print('Média das notas: %.2f' %media)
