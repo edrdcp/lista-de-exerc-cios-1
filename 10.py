90918cc9888d0eb1e62330c6ff13e41a03c6c7af
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 16:47:01 -0300

    1st v

---
+++ 10.py
@@ -0,0 +1,7 @@
+#10 Programa que pede a temperatura em graus Celsius, transforma e mostra em graus Fahrenheit.
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+tempC = float(input('Temperatura em graus Celsius: '))
+tempF = (tempC  * 1.8) + 32
+print('Temperatura em graus Fahrenheit: ', tempF)
