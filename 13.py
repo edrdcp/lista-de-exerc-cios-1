fe5063f3ef7f6da4e96305605a9557d635df2fd2
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 17:10:16 -0300

    1st v

---
+++ 13.py
@@ -0,0 +1,9 @@
+#13
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+h = float(input('Forneça sua altura em m: '))
+pesoIdealH = (72.7 * h) - 58
+pesoIdealM = (62.1 * h) - 44.7
+print('Se homem, seu peso ideal é: %.2f' %pesoIdealH)
+print('Se mulher, seu peso ideal é: %.2f' %pesoIdealM)
