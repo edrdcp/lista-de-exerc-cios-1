4f87cea81dc41ee765b7644bcb8d931530c22d04
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 16:40:55 -0300

    1st v

---
+++ 5.py
@@ -0,0 +1,7 @@
+#5 Programa que converte metros para centímetros.
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+m = float(input('Forneça a métrica para a conversão em centímetros: '))
+c = m * 100
+print(c, 'cm')
