b452b983cfad48dfe04dee2e12f956f2bbe40a95
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 16:55:34 -0300

    1st v

---
+++ 11.py
@@ -0,0 +1,15 @@
+#11
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+n1 = int(input('1º Forneça um número inteiro:'))
+n2 = int(input('2º Forneça outro número inteiro:'))
+n3 = float(input('3º Forneça um número real:'))
+
+produto = (n1 * 2) * (n2 / 2)
+soma = (n1 * 3) + n3
+potencia = n3 ** 3
+
+print('Produto do dobro do primeiro com metade do segundo: ', produto)
+print('Soma do triplo do primeiro com o terceiro: ', soma)
+print('Rerceiro elevado ao cubo %.2f' %potencia)
