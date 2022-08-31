e2c6d465b5e2dc19edcb4b4d4fa408e132b397d9
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-08-31 16:43:32 -0300

    1st v

---
+++ 7.py
@@ -0,0 +1,8 @@
+#7 Programa que calcula a área de um quadrado, em seguida mostra o dobro desta área para o usuário.
+#programador: Eduardo Costa
+#data da última atualização: 28/08/2022
+
+l = float(input('Forneça a medida do lado do quadrado: '))
+a = l ** 2
+
+print('O dobro da área do quadrado: ', a * 2)
